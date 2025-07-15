cda6d1cf8e5ba8ff61bd7ecc0f0ec2eb64c24c11
gjcline <188381159+gjcline@users.noreply.github.com>
2025-07-15 17:44:51 -0400

    Fix Solana RPC 403 error

--- src/components/WalletProvider.tsx
+++ src/components/WalletProvider.tsx
@@ -14,7 +14,7 @@ interface Props {
 
 export const WalletConnectionProvider: FC<Props> = ({ children }) => {
   const network = WalletAdapterNetwork.Mainnet;
-  const endpoint = useMemo(() => 'https://api.mainnet-beta.solana.com', []);
+  const endpoint = useMemo(() => 'https://solana-mainnet.phantom.app/', []);
   
   const wallets = useMemo(
     () => [

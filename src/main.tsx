bef3df481709ec6795d872f5d61bc66a78554c5c
gjcline <188381159+gjcline@users.noreply.github.com>
2025-07-17 01:35:35 -0400

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Buffer } from 'buffer';
+import Buffer from 'buffer';
 import App from './App.tsx';
 import './index.css';
 
